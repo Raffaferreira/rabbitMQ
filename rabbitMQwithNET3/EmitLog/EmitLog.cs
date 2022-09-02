4ce9762877c556eb346814e7c3e4cacd7807d1d7
Rafael Ferreira <Raffaferreira@users.noreply.github.com>
2022-09-01 23:31:27 -0300

    first commit

---
+++ rabbitMQwithNET3/EmitLog/EmitLog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+class EmitLog
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            // Envia para todas filas 
+            channel.ExchangeDeclare(exchange: "logs_exchange", type: ExchangeType.Fanout);
+
+            for(int i = 0; i < 25; i++) {
+
+                var message = GetMessage(args);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "logs_exchange",
+                                     routingKey: "",
+                                     basicProperties: null,
+                                     body: body);
+
+                Console.WriteLine(" [x] Sent {0}", message);
+
+                Thread.Sleep(2000);
+            }
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+
+    private static string GetMessage(string[] args)
+    {
+        return ((args.Length > 0)
+               ? string.Join(" ", args)
+               : "Hello World, Rafael!");
+    }
+}
---
+++ rabbitMQwithNET3/ReceiveLogs/ReceiveLogs.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+class ReceiveLogs
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            //channel.ExchangeDeclare(exchange: "logs_exchange", type: ExchangeType.Fanout);
+
+            //var queueName = channel.QueueDeclare().QueueName; //Random named queue created
+
+            channel.QueueDeclare(queue: "logs_queue",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            channel.QueueBind(queue: "logs_queue",
+                              exchange: "logs_exchange",
+                              routingKey: "");
+
+            Console.WriteLine(" [*] Waiting for logs.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] {0}", message);
+
+                Thread.Sleep(1000);
+            };
+
+            channel.BasicConsume(queue: "logs_queue",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
